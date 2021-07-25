866fa22c04c1b9ed1e950e8a4a1aa3df29f57c83
Max Base <MaxBaseCode@gmail.com>
2021-07-25 16:50:11 +0430

    improve ident server.ts

--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
 import express from 'express'
-  
+
 const app = express()
 const PORT:Number = 9898
-  
+
 app.get('/', (req, res) => {
-    res.send('Welcome to typescript backend!')
+	res.send('Welcome to typescript backend!')
 })
-  
+
 app.listen(PORT,() => {
 	console.log(`The application is listening on port http://localhost:${PORT}`)
 })
